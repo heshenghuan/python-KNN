38c3f225f4de9572917a562698fd2963c214f30a
heshenghuan <heshenghuan@sina.com>
2016-10-21 18:14:17 +0800

    commit __init__.py

---
+++ __init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Created on 17:46:01 2016-10-21
+
+@author: heshenghuan (heshenghuan@sina.com)
+http://github.com/heshenghuan
+"""
+
+__all__ = ['kdtree', 'knn']
